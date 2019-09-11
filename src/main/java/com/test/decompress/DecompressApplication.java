1d3c590718570aae1a03bf6fef1578cbcc9a533d
fammoko <fammoko@gmail.com>
2019-09-11 12:39:37 +0700

    First create.

---
+++ src/main/java/com/test/decompress/DecompressApplication.java
@@ -0,0 +1,13 @@
+package com.test.decompress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecompressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecompressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/decompress/DecompressApplicationTests.java
@@ -0,0 +1,16 @@
+package com.test.decompress;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DecompressApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
